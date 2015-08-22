f5e70d3cd0da3ab226fec16b4c62c226385047cc
Alan Sangma <alan@whitefrog.org>
2015-08-22 14:37:29 -0700

    added conditional for localhost and database connections

--- include/config.php
+++ include/config.php
@@ -21,6 +21,8 @@ $dbpassword = "root";*/
 	
 
 //$dbhost = "inlandempireweightloss.com";
+
+
 $dbhost = 'localhost';
 $dbuser  = "cknopke_iewlusr";
 //$dbpassword = "yukQtrGX7vyiEh9";
@@ -28,4 +30,9 @@ $dbpassword = "GsXIseZZmjX83Ji";
 
 $dbdatabase = "cknopke_iewlcms";
 
+if($_SERVER['SERVER_NAME'] == 'localhost') {
+	$dbhost = "localhost";
+	$dbuser  = "root";
+	$dbpassword = "root";
+}
 ?>
