9a1b3b708025db9497939bd866f1146fac9daa6e
Alan Sangma <alansangma@gmail.com>
2015-10-04 14:55:23 -0700

    weightloss page 80% done : need to add Problems Treated page for the links under the Obesity tab

--- tabbed-content/forms-content.php
+++ tabbed-content/forms-content.php
@@ -1,33 +1,41 @@
 <div class="col-md-12">
 	<div class="forms-wrap">
-      <div id="forms" class="col-xs-3" style="text-align:center;">
-        <a href="documents/NewPatientWeightLossPacket.pdf" title="New Patient Weight Loss Packet" target="_blank" class="pdf"><img src="graphics/pdf-icon.png" title="New Patient Weight Loss Packet" width="60" height="60"></a>
-        <a href="documents/NewPatientWeightLossPacket.pdf" title="New Patient Weight Loss Packet" target="_blank" class="pdf_link">Weight Loss<br>Patient Packet</a>
+      <div class="col-xs-3 ">
+        <div class="form-module">
+          <a href="documents/NewPatientWeightLossPacket.pdf" title="New Patient Weight Loss Packet" target="_blank" class="pdf"><i class="fa fa-file-pdf-o fa-4x"></i></a>
+          <a href="documents/NewPatientWeightLossPacket.pdf" title="New Patient Weight Loss Packet" target="_blank" class="pdf_link">Weight Loss<br>Patient Packet</a>
+        </div>
         <hr>
         <p>Please come to your appointment with this filled out to speed up the check in process.</p>
       </div>
-      <div class="col-xs-3" style="text-align:center;">
-        <a href="documents/NewPatientPacket.pdf" title="New Patient Packet" target="_blank" class="pdf"><img src="graphics/pdf-icon.png" title="New Patient Packet" width="60" height="60"></a>
-        <a href="documents/NewPatientPacket.pdf" title="New Patient Packet" target="_blank" class="pdf_link">New Patient<br>Packet</a>
-        <hr>
-        <p>This packet is used for our non-weight loss patients.  Please come to your appointment with this filled out to speed up the check in process.</p>
-      </div>
-      <div class="col-xs-3" style="text-align:center;">
-        <a href="documents/InformedConsentWeightLoss.pdf" title="Informed Consent" target="_blank" class="pdf"><img src="graphics/pdf-icon.png" title="Informed Consent" width="60" height="60"></a>
-        <a href="documents/InformedConsentWeightLoss.pdf" title="Informed Consent" target="_blank" class="pdf_link">Informed Consent</a>
+      <div class="col-xs-3 ">
+        <div class="form-module">
+          <a href="documents/InformedConsentWeightLoss.pdf" title="Informed Consent" target="_blank" class="pdf"><i class="fa fa-file-pdf-o fa-4x"></i></a>
+          <a href="documents/InformedConsentWeightLoss.pdf" title="Informed Consent" target="_blank" class="pdf_link">Informed Consent</a>
+        </div>
         <hr>
         <p>You can prefill this out or review it at home to understand the risks and benefits to the weight loss process.</p>
       </div>
-      <div class="col-xs-3" style="text-align:center;" id="guidelines">
-        <a href="documents/ASBPGuidelinesForOverweightAndObesityEvaluationManagement.pdf" title="ASBP Guidelines For Overweight And Obesity Evaluation Management" target="_blank" class="pdf"><img src="graphics/pdf-icon.png" title="ASBP Guidelines For Overweight And Obesity Evaluation Management" width="60" height="60"></a>
-        <a href="documents/ASBPGuidelinesForOverweightAndObesityEvaluationManagement.pdf" title="ASBP Guidelines For Overweight And Obesity Evaluation Management" target="_blank" class="pdf_link">ASBP Guidelines</a>
+      <div class="col-xs-3 ">
+        <div class="form-module">
+          <a href="documents/ASBPGuidelinesForOverweightAndObesityEvaluationManagement.pdf" title="ASBP Guidelines For Overweight And Obesity Evaluation Management" target="_blank" class="pdf"><i class="fa fa-file-pdf-o fa-4x"></i></a>
+          <a href="documents/ASBPGuidelinesForOverweightAndObesityEvaluationManagement.pdf" title="ASBP Guidelines For Overweight And Obesity Evaluation Management" target="_blank" class="pdf_link">ASBP Guidelines</a>
+        </div>
         <hr>
         <p>If you are curious about the history of the American Society of Bariatric Physicians and their position statement on the assessment and management of obesity and related conditions, please read this document.  It will help to answer questions about the safety and reasoning behind different treatment options in obesity medicine.</p>
         <div class="clear tall_20"></div>
       </div>
+      <div class="col-xs-3 ">
+        <div class="form-module">
+          <a href="documents/NewPatientPacket.pdf" title="New Patient Packet" target="_blank" class="pdf"><i class="fa fa-file-pdf-o fa-4x"></i></a>
+          <a href="documents/NewPatientPacket.pdf" title="New Patient Packet" target="_blank" class="pdf_link">New Patient<br>Packet</a>
+        </div>
+        <hr>
+        <p>This packet is used for our non-weight loss patients.  Please come to your appointment with this filled out to speed up the check in process.</p>
+      </div>
     </div>
 </div>
-<div class="col-md-12 text-center" style="margin-top:40px;">
+<div class="col-md-8" style="margin-top:40px;">
 	<h5>Get Adobe Acrobat Reader</h5>
-	<p>If you are having trouble opening the forms, you may need to download and install Adobe Acrobat Reader.<br/>Just click <a href="http://get.adobe.com/reader/" target="_blank">here</a> and you will be taken to a page where you can download Adobe Acrobat Reader.</p>
+	<p>If you are having trouble opening the forms, you may need to download and install Adobe Acrobat Reader. Just click <a href="http://get.adobe.com/reader/" target="_blank">here</a> and you will be taken to a page where you can download Adobe Acrobat Reader.</p>
 </div>
